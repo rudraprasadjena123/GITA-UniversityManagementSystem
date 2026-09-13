cc45596d8cd2dbd65a108765d1fb92196a332588
rudraprasadjena123 <rudrajena814@gmail.com>
2026-09-13 11:33:42 +0530

    Uploading this project From VS Code in git hub

--- University Management System/src/university/management/system/About.java
+++ University Management System/src/university/management/system/About.java
@@ -22,17 +22,17 @@ public class About extends JFrame {
         heading.setFont(new Font("Tahoma", Font.BOLD, 30));
         add(heading);
         
-        JLabel name = new JLabel("Developed By: Code for Interview");
+        JLabel name = new JLabel("Developed By: Rudra Prasad Jena");
         name.setBounds(70, 220, 550, 40);
         name.setFont(new Font("Tahoma", Font.BOLD, 30));
         add(name);
         
-        JLabel rollno = new JLabel("Roll number: 1533146");
+        JLabel rollno = new JLabel("Roll number: 2307084");
         rollno.setBounds(70, 280, 550, 40);
         rollno.setFont(new Font("Tahoma", Font.PLAIN, 30));
         add(rollno);
         
-        JLabel contact = new JLabel("Contact: codeforinterview03@gmail.com");
+        JLabel contact = new JLabel("Contact: rudraprasad0@gmail.com");
         contact.setBounds(70, 340, 550, 40);
         contact.setFont(new Font("Tahoma", Font.PLAIN, 20));
         add(contact);
--- University Management System/src/university/management/system/Conn.java
+++ University Management System/src/university/management/system/Conn.java
@@ -10,7 +10,7 @@ public class Conn {
     Conn () {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql:///universitymanagementsystem", "root", "codeforinterview");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/universitymanagementsystem", "root", "rudra@7894830138");
             s = c.createStatement();
             
         } catch (Exception e) {
